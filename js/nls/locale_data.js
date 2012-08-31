6d0fb28b9fadefa17476c33f834bcd7dc53b6ce9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-08-31 12:48:57 +0200

    Root translations as module.

--- js/nls/locale_data.js
+++ js/nls/locale_data.js
@@ -1,13 +1,4 @@
 define({
-    "root": {
-        "messages" : {
-            "" : {
-                "domain" : "messages",
-                "lang"   : "en",
-                "plural_forms" : "nplurals=2; plural=(n != 1);"
-            },
-            "Beer is" : [ null, "Beer is"]
-        }
-    },
+    "root": true, // Root = en
     "nl": true
 });
